8b556c45bfc52e81a430e66b357eac314f62de22
chernikov-frontend <130126797+chernikov-frontend@users.noreply.github.com>
2023-11-19 14:44:15 +0400

    задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - (4 * a * c)
+  let root1;
+  let root2;
+
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant = 0) {
+    root1 = (-b) / (2 * a);
+    arr.push(root1);
+  } else if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(d) )/(2*a);
+    root2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(root1, root2);
+  }
   
   return arr;
 }
